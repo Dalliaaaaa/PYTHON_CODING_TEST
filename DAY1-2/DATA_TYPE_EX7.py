bd923fff0128a5996f18c52c7f9af74604ef732b
김주미 <ajswlsoato0619@gmail.com>
2023-07-13 10:21:19 +0000

    DAY2 도전 문제 완료

--- DAY1-2/DATA_TYPE_EX7.py
+++ DAY1-2/DATA_TYPE_EX7.py
@@ -1,5 +1,11 @@
-a = []
+result = [] #리스트 선언
 
 while True:
-    arr = input()
-    if 
+    arr = input("내용을 입력하세요:")
+    if arr == '':
+        break
+    else:
+        result.append(arr)
+    
+result = [i for i in result]
+print(result)
